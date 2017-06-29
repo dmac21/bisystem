90f042818b1b77165d950c1cf81c962fa769c463
dmac21 <386501732@qq.com>
2017-06-29 23:49:02 +0800

    调整了登录界面

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
+#coding:utf-8
 from . import auth
 from .forms import LoginForm
-from flask import render_template,redirect,url_for,request
+from flask import render_template,redirect,url_for,request,flash
 from flask_login import login_user,logout_user,login_required,current_user
 from ..models import  User
 
@@ -12,4 +13,12 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user,form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
+        flash(u'无效的用户名或者密码')
     return render_template('auth/login.html',form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(u'你已成功退出系统')
+    return redirect(url_for('main.index'))
