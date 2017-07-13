583e184db03396771511817e6cc2628cf6518595
dmac21 <386501732@qq.com>
2017-07-13 18:13:28 +0800

    增加了echart图表

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,13 @@
+#coding:utf-8
 from . import main
 from flask import render_template
 from flask_login import login_required, current_user
+import MySQLdb as mysql
+import json,datetime
+
+database=mysql.connect(user='root',passwd='root',db='wechat_data2',charset='utf8')
+cursor=database.cursor()
+
 
 @main.route('/')
 def index():
@@ -8,7 +15,13 @@ def index():
 
 @main.route('/wechat')
 def wechat():
-    return render_template('wechat.html')
+    sql="select ref_date,sum(cumulate_user) cumulate_user from usercumulate_data where ref_date>='2017-07-01' group by ref_date"
+    cursor.execute(sql)
+    data =cursor.fetchall()
+    jsonstr={}
+    jsonstr['ref_date']=[x[0].strftime('%Y-%m-%d') for x in data]
+    jsonstr['cumulate_user'] = [int(x[1]) for x in data]
+    return render_template('wechat.html',data=jsonstr)
 
 @main.route('/sysmanage')
 def sysmanage():
